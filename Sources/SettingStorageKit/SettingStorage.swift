b93f9226dbec3a3d7703efc274b641d48bfbe937
FeliksLv <felikslv@163.com>
2025-01-19 04:18:37 +0800

    feat: AppSettings Support clearAll

--- Sources/SettingStorageKit/SettingStorage.swift
+++ Sources/SettingStorageKit/SettingStorage.swift
@@ -9,69 +9,75 @@ import Foundation
 @preconcurrency import MMKV
 
 public struct AppSettings: Sendable {
-    public struct Keys : Hashable, Equatable, RawRepresentable, @unchecked Sendable {
+    public struct Keys: Hashable, Equatable, RawRepresentable, @unchecked Sendable {
         public var rawValue: String
         public init(rawValue: String) {
             self.rawValue = rawValue
         }
     }
-    
-    private static var mmkv: MMKV? = {
-        return MMKV(mmapID: "__global__")
-    }()
-    
+
+    private static var mmkv: MMKV? = MMKV(mmapID: "__global__")
+
     @propertyWrapper
     public struct Storage<T: Codable> {
         let key: Keys
         let defaultValue: T
         public var wrappedValue: T {
-            get { MMKVAccessor.value(forKey: key.rawValue, defaultValue: defaultValue, mmkv: AppSettings.mmkv) }
+            get {
+                MMKVAccessor.value(
+                    forKey: key.rawValue, defaultValue: defaultValue, mmkv: AppSettings.mmkv)
+            }
             set { MMKVAccessor.set(newValue, forKey: key.rawValue, mmkv: AppSettings.mmkv) }
         }
-        
+
         public init(key: Keys, defaultValue: T) {
             self.defaultValue = defaultValue
             self.key = key
         }
-        
+
         public init(key: Keys) where T: ExpressibleByNilLiteral {
             self.key = key
             self.defaultValue = nil
         }
     }
-    
+
     public static func set<T: Codable>(_ value: T, forKey key: String) {
         MMKVAccessor.set(value, forKey: key, mmkv: AppSettings.mmkv)
     }
-    
+
     public static func value<T: Codable>(forKey key: String, defaultValue: T) -> T {
         return MMKVAccessor.value(forKey: key, defaultValue: defaultValue, mmkv: AppSettings.mmkv)
+    }
+
+    public static func clearAll() {
+        MMKVAccessor.clearAll(mmkv: AppSettings.mmkv)
     }
 }
 
 public class UserSettings: NSObject {
     private static var mmkv: MMKV?
-    public struct Keys : Hashable, Equatable, RawRepresentable, @unchecked Sendable {
+    public struct Keys: Hashable, Equatable, RawRepresentable, @unchecked Sendable {
         public var rawValue: String
         public init(rawValue: String) {
             self.rawValue = rawValue
         }
     }
-    
+
     @propertyWrapper
     public struct Storage<T: Codable> {
         public init(key: Keys, defaultValue: T) {
             self.key = key
             self.defaultValue = defaultValue
         }
-        
+
         let key: Keys
         let defaultValue: T
-        
+
         public var wrappedValue: T {
             get {
                 assert(initializedFlag)
-                return MMKVAccessor.value(forKey: key.rawValue, defaultValue: defaultValue, mmkv: UserSettings.mmkv)
+                return MMKVAccessor.value(
+                    forKey: key.rawValue, defaultValue: defaultValue, mmkv: UserSettings.mmkv)
             }
             set {
                 assert(initializedFlag)
@@ -79,33 +85,35 @@ public class UserSettings: NSObject {
             }
         }
     }
-    
+
     private static var initializedFlag = false
     private static let initializedOnce: () = {
         initializedFlag = true
     }()
-    
+
     @objc public static func changeUser(with userId: String?) {
         assert(Thread.isMainThread)
-        
+
         let _ = initializedOnce
-        
+
         if let userId = userId {
-            self.mmkv = MMKV(mmapID: "user.\(userId)")
+            mmkv = MMKV(mmapID: "user.\(userId)")
         } else {
-            self.mmkv = nil
+            mmkv = nil
         }
     }
-    
+
     // 防止崩溃（默认的实现会引起 NSUndefinedKeyException）
-    public override class func value(forUndefinedKey key: String) -> Any? {
+    override public class func value(forUndefinedKey key: String) -> Any? {
         let errorMessage = "UserSettings: Cannot Find Value for UndefinedKey <\(key)>"
-        assert(false, errorMessage)
+        assertionFailure(errorMessage)
         return nil
     }
-    public override class func setValue(_ value: Any?, forUndefinedKey key: String) {
-        let errorMessage = "UserSettings: Cannot Set Value <\(String(describing: value))> for UndefinedKey <\(key)>"
-        assert(false, errorMessage)
+
+    override public class func setValue(_ value: Any?, forUndefinedKey key: String) {
+        let errorMessage =
+            "UserSettings: Cannot Set Value <\(String(describing: value))> for UndefinedKey <\(key)>"
+        assertionFailure(errorMessage)
     }
 }
 
@@ -121,13 +129,13 @@ private struct MMKVAccessor {
             assertionFailure()
             return
         }
-        guard let _ = value else {
+        guard value != nil else {
             mmkv.removeValue(forKey: key)
             return
         }
-        
+
         // 类型处理
-        
+
         if let value = value as? Bool {
             mmkv.set(value, forKey: key)
         } else if let value = value as? Int {
@@ -162,16 +170,16 @@ private struct MMKVAccessor {
             }
         }
     }
-    
+
     static func value<T: Codable>(forKey key: String, defaultValue: T, mmkv: MMKV?) -> T {
         guard let mmkv = mmkv else {
             return defaultValue
         }
-        
+
         if !mmkv.contains(key: key) {
             return defaultValue
         }
-        
+
         if T.self == Int.self || T.self == Int?.self {
             return Int(mmkv.int64(forKey: key)) as? T ?? defaultValue
         } else if T.self == Int64.self || T.self == Int64?.self {
@@ -210,5 +218,9 @@ private struct MMKVAccessor {
                 return defaultValue
             }
         }
+    }
+
+    static func clearAll(mmkv: MMKV?) {
+        mmkv?.clearAll()
     }
 }
